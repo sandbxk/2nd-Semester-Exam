ae6873306d2b843b83dac11a6fde59ba158d40fd
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-01 12:51:12 +0200

    Initial Setup

---
+++ src/main/java/Application/DAL/DBConnector/DBConnection.java
@@ -0,0 +1,35 @@
+package Application.DAL.DBConnector;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DBConnection {
+    private SQLServerDataSource dataSource;
+    private Properties property;
+
+    public DBConnection() throws IOException
+    {
+        property = new Properties();
+        dataSource = new SQLServerDataSource();
+
+        try {
+            property.load(new FileReader("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dataSource.setServerName(property.getProperty("server"));
+        dataSource.setDatabaseName(property.getProperty("database"));
+        dataSource.setUser(property.getProperty("username"));
+        dataSource.setPassword(property.getProperty("password"));
+    }
+
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
+}
---
+++ src/main/java/Application/DAL/TempDau.java
@@ -0,0 +1,8 @@
+package Application.DAL;
+
+public class TempDau {
+    /**
+     * This is just here there can be a package inside of a package, thanks IntelliJ.
+     * Delete once actual DAU classes are put in place.
+     */
+}
