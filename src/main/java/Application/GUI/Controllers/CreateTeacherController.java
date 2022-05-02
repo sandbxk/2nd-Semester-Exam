ff289ca77d8837fd4bd5d275d78d33a5c1f2ea00
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-02 12:50:31 +0200

    Controller and FXML update

---
+++ src/main/java/Application/GUI/Controllers/CreateTeacherController.java
@@ -0,0 +1,20 @@
+package Application.GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class CreateTeacherController
+{
+    @FXML public TextField txtFieldLogin;
+    @FXML public TextField txtFieldPassword;
+    @FXML public TextField txtFieldName;
+    @FXML public TextField txtFieldEmail;
+
+    public void onSaveCreateTeacher(ActionEvent actionEvent) {
+    }
+
+    public void onCancelCreateTeacher(ActionEvent actionEvent) {
+    }
+}
