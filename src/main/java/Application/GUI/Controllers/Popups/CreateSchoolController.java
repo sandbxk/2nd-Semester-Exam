16ba10e733c30d4050c41f1ffe5e8f1f5eb1259d
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-05-03 11:00:57 +0200

    dataflow and adminview update

--- src/main/java/Application/GUI/Controllers/Popups/CreateSchoolController.java
+++ src/main/java/Application/GUI/Controllers/Popups/CreateSchoolController.java
@@ -1,14 +1,18 @@
 package Application.GUI.Controllers.Popups;
 
+import Application.DAL.mockSchoolDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
 
 public class CreateSchoolController implements Initializable {
 
@@ -19,18 +23,39 @@ public class CreateSchoolController implements Initializable {
     @FXML
     private Button btnCancel;
 
+    private mockSchoolDAO schoolDAO;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        txtSchoolZipCode.setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter(), 0, integerFilter()));
     }
 
     public void createSchool(ActionEvent actionEvent) {
+        schoolDAO.createSchool(txtsSchoolName.getText(),txtSchoolZipCode.getText());
 
     }
 
     public void Cancel(ActionEvent actionEvent) {
         Stage stage = (Stage) btnCancel.getScene().getWindow();
         stage.close();
+    }
+
+    /**
+     * An interger filter, for use in a textFormatter. Only allows whole numbers.
+     * @return
+     */
+    private UnaryOperator<TextFormatter.Change> integerFilter(){
+        UnaryOperator<TextFormatter.Change> integerFilter = change -> {
+            String newText = change.getControlNewText();
+            //if (newText.matches("-?([1-9][0-9]*)?")) {
+            if (newText.matches("-?([1-9][0-9]*)?")) {
+
+                return change;
+            }
+            return null;
+        };
+
+        return integerFilter;
     }
 }
