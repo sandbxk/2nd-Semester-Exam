375640e0470994705d8e616b00a1d1a980018305
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-03 15:44:30 +0200

    addTeacher view should now work correctly

--- src/main/java/Application/BE/Teacher.java
+++
@@ -1,4 +0,0 @@
-package Application.BE;
-
-public record Teacher(int id, String login, String password, String firstName, String surname, String email, String salt, int schoolid) {
-}
---
+++ src/main/java/Application/GUI/Controllers/Popups/CreateTeacherController.java
@@ -0,0 +1,50 @@
+package Application.GUI.Controllers.Popups;
+
+import Application.BE.School;
+import Application.BLL.AdminDataManager;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateTeacherController implements Initializable
+{
+    @FXML public TextField txtFieldLogin;
+    @FXML public TextField txtFieldPassword;
+    @FXML public TextField txtFieldFirstName;
+    @FXML public TextField txtFieldSurname;
+    @FXML public TextField txtFieldEmail;
+
+    private AdminDataManager adminDataManager;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        adminDataManager = new AdminDataManager();
+    }
+
+    public void onSaveCreateTeacher(ActionEvent actionEvent) {
+
+        // FIXME: 03/05/2022 -- Dummy School
+        School school = new School(-1, "Dummy School", 6700, "NotARealCity");
+
+        adminDataManager.createTeacher(
+                txtFieldLogin.getText(),
+                txtFieldPassword.getText(),
+                txtFieldFirstName.getText(),
+                txtFieldSurname.getText(),
+                txtFieldEmail.getText(),
+                school
+        );
+
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+    }
+
+    public void onCancelCreateTeacher(ActionEvent actionEvent)
+    {
+        ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+    }
+}
