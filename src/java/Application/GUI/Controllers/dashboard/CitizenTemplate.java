8a6669b548d21649292e89542c1736cb4c15e321
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-05-05 13:57:57 +0200

    Update CitizenTemplate.java

--- src/java/Application/GUI/Controllers/dashboard/CitizenTemplate.java
+++ src/java/Application/GUI/Controllers/dashboard/CitizenTemplate.java
@@ -1,10 +1,21 @@
 package Application.GUI.Controllers.dashboard;
 
+import Application.GUI.Models.CategoryEntryModel;
+import Application.GUI.Models.CitizenTemplateModel;
+import Application.GUI.Models.ControllerModels.TeacherViewModel;
+import Application.GUI.Models.FunctionalLevels;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
+import java.time.format.DateTimeFormatter;
+
 public class CitizenTemplate {
+
     public AnchorPane anchorPaneCitizenTemplateDashboard;
     public ListView listViewCitizenTemplates;
     public TextField txtFieldCitizenTemplateSearch;
@@ -22,9 +33,32 @@ public class CitizenTemplate {
     public Button btnCitizenTemplateChangeJournal;
     public ToggleButton tglBtnCitizenTemplateEditOn;
     public ToggleButton tglBtnCitizenTemplateEditOff;
+
+
+    // Citizen Template - Functional Conditions
+    public TreeTableView<CategoryEntryModel> treeTblViewFunc;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncCategory;
+    public TreeTableColumn<CategoryEntryModel, ComboBox<FunctionalLevels>> treeTblColumnFuncLevel;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncAssessment;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncCause;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncImplications;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncCitizenGoals;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncExpectedCondition;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnFuncNote;
+
+    // Citizen Template - Health Conditions
+    public TreeTableView<CategoryEntryModel> treeTblViewHealth;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnHealthCategory;
+    public TreeTableColumn<CategoryEntryModel, Number> treeTblColumnHealthLevel;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnHealthAssessment;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnHealthCause;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnHealthExpectedCondition;
+    public TreeTableColumn<CategoryEntryModel, String> treeTblColumnHealthNote;
+
+    // Citizen Template - General Information
     public TextArea txtAreaGenInfoMastering;
     public TextArea txtAreaGenInfoMotivation;
-    public TextArea txtAreaGenInfoResources;
+    public TextArea txtAreaGenInfoRessources;
     public TextArea txtAreaGenInfoRoles;
     public TextArea txtAreaGenInfoHabits;
     public TextArea txtAreaGenInfoEduAndJob;
@@ -32,42 +66,149 @@ public class CitizenTemplate {
     public TextArea txtAreaGenInfoHealthInfo;
     public TextArea txtAreaGenInfoAssistiveDevices;
     public TextArea txtAreaGenInfoHomeLayout;
-    public Label txtAreaGenInfoNetwork;
-    
-    public TreeTableView treeTblViewHealth;
-    
-    public TreeTableColumn treeTblColumnHealthCategory;
-    public TreeTableColumn treeTblColumnHealthLevel;
-    public TreeTableColumn treeTblColumnHealthAssessment;
-    public TreeTableColumn treeTblColumnHealthCause;
-    public TreeTableColumn treeTblColumnHealthExpectedCondition;
-    public TreeTableColumn treeTblColumnHealthNote;
-    
-    public TreeTableColumn treeTblColumnFuncCategory;
-    public TreeTableColumn treeTblColumnFuncLevel;
-    public TreeTableColumn treeTblColumnFuncAssessment;
-    public TreeTableColumn treeTblColumnFuncCause;
-    public TreeTableColumn treeTblColumnFuncImplications;
-    public TreeTableColumn treeTblColumnFuncCitizenGoals;
-    public TreeTableColumn treeTblColumnFuncExpectedCondition;
-    public TreeTableColumn treeTblColumnFuncNote;
+    public TextArea txtAreaGenInfoNetwork;
+
+    private BooleanProperty citizenTemplateEditMode = new SimpleBooleanProperty(false);
+    private ToggleGroup toggleGroup;
+    private TeacherViewModel teacherViewModel = new TeacherViewModel();
 
     public void onCitizenTemplateSearch(ActionEvent event) {
-    }
-
-    public void onAddCitizenTemplateContactInfo(ActionEvent event) {
+        teacherViewModel.citizenTemplateSearch();
     }
 
     public void onRemoveCitizenTemplateContactInfo(ActionEvent event) {
+        teacherViewModel.removeCitizenTemplateContactInfo();
     }
 
-    public void onCitizenTemplateEditBaseData(ActionEvent event) {
-    }
-
-    public void onCitizenTemplateChangeJournal(ActionEvent event) {
+    public void onAddCitizenTemplateContactInfo(ActionEvent event) {
+        teacherViewModel.addCitizenTemplateContactInfo();
     }
 
     private void setFuncTreeTable(){
+        // Set up the table
+        CitizenTemplateModel citizenTemplateModel = new CitizenTemplateModel();
+
+        ObservableList<CategoryEntryModel> func = citizenTemplateModel.getFunctionalAbilities();
+        ObservableList<CategoryEntryModel> health = citizenTemplateModel.getHealthConditions();
+
+        ObservableList<TreeItem<CategoryEntryModel>> funcTree = FXCollections.observableArrayList();
+        ObservableList<TreeItem<CategoryEntryModel>> healthTree = FXCollections.observableArrayList();
+
+        TreeItem<CategoryEntryModel> funcRoot = new TreeItem<>(new CategoryEntryModel("Functional Abilities"));
+        TreeItem<CategoryEntryModel> healthRoot = new TreeItem<>(new CategoryEntryModel("Health Conditions"));
+        treeTblViewFunc.setRoot(funcRoot);
+        treeTblViewHealth.setRoot(healthRoot);
+
+        for (CategoryEntryModel categoryEntryModel : func) {
+            funcTree.add(new TreeItem<>(categoryEntryModel));
+        }
+
+        for (CategoryEntryModel categoryEntryModel : health) {
+            healthTree.add(new TreeItem<>(categoryEntryModel));
+        }
+        funcRoot.getChildren().addAll(funcTree);
+        healthRoot.getChildren().addAll(healthTree);
+
         //https://jenkov.com/tutorials/javafx/treetableview.html
     }
+
+    private void initToggleGroup()
+    {
+
+        toggleGroup = new ToggleGroup();
+        tglBtnCitizenTemplateEditOn.setToggleGroup(toggleGroup);
+        tglBtnCitizenTemplateEditOff.setToggleGroup(toggleGroup);
+    }
+
+    private void initTreeTableClmns(){
+        treeTblColumnFuncCategory.setCellValueFactory(param -> param.getValue().getValue().categoryNameProperty());
+        treeTblColumnFuncLevel.setCellValueFactory(param -> param.getValue().getValue().getLevelImageComboBoxProperty());
+        treeTblColumnFuncAssessment.setCellValueFactory(param -> param.getValue().getValue().assessmentProperty());
+        treeTblColumnFuncCause.setCellValueFactory(param -> param.getValue().getValue().causeProperty());
+        treeTblColumnFuncImplications.setCellValueFactory(param -> param.getValue().getValue().implicationsProperty());
+        treeTblColumnFuncCitizenGoals.setCellValueFactory(param -> param.getValue().getValue().citizenGoalsProperty());
+        treeTblColumnFuncExpectedCondition.setCellValueFactory(param -> param.getValue().getValue().expectedConditionProperty());
+        treeTblColumnFuncNote.setCellValueFactory(param -> param.getValue().getValue().noteProperty());
+
+        treeTblColumnHealthCategory.setCellValueFactory(param -> param.getValue().getValue().categoryNameProperty());
+        treeTblColumnHealthLevel.setCellValueFactory(param -> param.getValue().getValue().levelProperty());
+        treeTblColumnHealthAssessment.setCellValueFactory(param -> param.getValue().getValue().assessmentProperty());
+        treeTblColumnHealthCause.setCellValueFactory(param -> param.getValue().getValue().causeProperty());
+        treeTblColumnHealthExpectedCondition.setCellValueFactory(param -> param.getValue().getValue().expectedConditionProperty());
+        treeTblColumnHealthNote.setCellValueFactory(param -> param.getValue().getValue().noteProperty());
+    }
+
+    public void onCitizenTemplateChangeJournal(ActionEvent event) {
+        teacherViewModel.citizenTemplateChangeJournal();
+    }
+
+    public void onCitizenTemplateEditBaseData(ActionEvent event) {
+        teacherViewModel.onCitizenTemplateEditBaseData();
+    }
+
+    private void initCitizenTemplatesList(){
+        listViewCitizenTemplates.setItems(teacherViewModel.getCitizenTemplates());
+        listViewCitizenTemplates.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            teacherViewModel.setSelectedCitizenTemplateModel((CitizenTemplateModel) newValue);
+            setDataToCitizenTemplateView();
+        });
+    }
+
+    private void setDataToCitizenTemplateView(){
+        lblCitizenTemplateName.setText(teacherViewModel.getSelectedCitizenTemplateModel().getName());
+        lblAgeCitizenTemplate.setText(teacherViewModel.getSelectedCitizenTemplateModel().getAge() + "");
+        lblAddressCitizenTemplate.setText(teacherViewModel.getSelectedCitizenTemplateModel().getAddress());
+        lblBirthdateCitizenTemplate.setText(teacherViewModel.getSelectedCitizenTemplateModel().getBirthDate().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        lblHelpStatusCitizenTemplate.setText(teacherViewModel.getSelectedCitizenTemplateModel().getHelpStatus());
+        lblCivilianStatusCitizenTemplate.setText(teacherViewModel.getSelectedCitizenTemplateModel().getCivilianStatus());
+
+        listViewCitizenTemplateContactInfo.setItems(teacherViewModel.getSelectedCitizenTemplateModel().getContactInfo());
+    }
+
+    private void editModeListener(){
+        toggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) ->
+        {
+            ToggleButton newToggleValue = null;
+            ToggleButton oldToggleValue = null;
+            if(newValue != null) {
+                newToggleValue = (ToggleButton) newValue;
+                newToggleValue.setDisable(true);
+            }
+
+            if (oldValue != null) {
+                oldToggleValue = (ToggleButton) oldValue;
+                oldToggleValue.setDisable(false);
+            }
+
+            if(newToggleValue == tglBtnCitizenTemplateEditOn && newToggleValue != null)
+            {
+
+            }
+            else if(newToggleValue == tglBtnCitizenTemplateEditOff && newToggleValue != null) {
+
+            }
+
+
+        });
+    }
+
+    private void editModePropertyListener() {
+        citizenTemplateEditMode.addListener((observable, oldValue, newValue) -> editComboBoxes(newValue));
+
+        //TODO: Switch table items
+    }
+
+    private void editComboBoxes(boolean editable) {
+        treeTblViewFunc.setEditable(editable);
+        treeTblViewHealth.setEditable(editable);
+        for (TreeItem<CategoryEntryModel> cat : treeTblViewFunc.getRoot().getChildren()) {
+            cat.getValue().getLevelImageComboBox().setDisable(editable);
+        }
+        for (TreeItem<CategoryEntryModel> cat : treeTblViewHealth.getRoot().getChildren()) {
+            cat.getValue().getLevelImageComboBox().setDisable(editable);
+        }
+
+    }
+
+
 }
