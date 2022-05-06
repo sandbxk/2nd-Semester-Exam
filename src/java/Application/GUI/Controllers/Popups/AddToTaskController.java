a7279057a73fa2c9bed2db2b0b221593b007898e
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-06 10:40:22 +0200

    GUI update, added classes

---
+++ src/java/Application/GUI/Controllers/Popups/AddToTaskController.java
@@ -0,0 +1,31 @@
+package Application.GUI.Controllers.Popups;
+
+import javafx.event.ActionEvent;
+
+public class AddToTaskController {
+
+    public AddToTaskController()
+    {
+
+    }
+
+    public void onSave(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void onCancel(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void addAttendeeToList(ActionEvent actionEvent)
+    {
+
+    }
+
+    public void removeAccountFromList(ActionEvent actionEvent)
+    {
+
+    }
+}
