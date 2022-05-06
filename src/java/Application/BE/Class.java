ae89be9bdb37ed8eba537f28f11dbba6dab97ecf
Mads Mandahl-Barth <mads-barth@live.dk>
2022-05-06 11:12:29 +0200

    added annotations on be entities to translate between java and SQL much easier

--- src/java/Application/BE/Class.java
+++ src/java/Application/BE/Class.java
@@ -1,15 +1,35 @@
 package Application.BE;
 
+import Application.DAL.Annotations.SQLColumn;
+import Application.DAL.Annotations.SQLTable;
+
+@SQLTable(name = "classes")
 public class Class {
 
+    @SQLColumn(name = "classId")
     private int id;
+
+    @SQLColumn(name = "className")
     private String className;
+
+    @SQLColumn(name = "teacherID")
+    private int teacherID;
+
+    @SQLColumn(name = "school")
+    private int schoolID;
 
     public Class(int id, String className)
     {
         this.className = className;
         this.id = id;
+    }
 
+    public Class(int id, String className, int teacherID, int schoolID)
+    {
+        this.className = className;
+        this.id = id;
+        this.teacherID = teacherID;
+        this.schoolID = schoolID;
     }
 
     public int getId()
@@ -30,5 +50,21 @@ public class Class {
     public void setName(String name)
     {
         this.className = name;
+    }
+
+    public int getSchoolID() {
+        return schoolID;
+    }
+
+    public void setSchoolID(int schoolID) {
+        this.schoolID = schoolID;
+    }
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public void setTeacherID(int teacherID) {
+        this.teacherID = teacherID;
     }
 }
