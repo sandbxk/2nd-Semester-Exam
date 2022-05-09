352a408d811d8887b7e8e8c81c49a3a761ead28e
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-05-09 19:03:10 +0200

    Case update

--- src/java/Application/GUI/Models/InquiryModel.java
+++ src/java/Application/GUI/Models/InquiryModel.java
@@ -2,16 +2,24 @@ package Application.GUI.Models;
 
 import Application.BE.Inquiry;
 import Application.BLL.TeacherDataManager;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class InquiryModel
 {
     TeacherDataManager teacherDataManager = new TeacherDataManager();
 
+    private IntegerProperty id;
     private StringProperty inquiryName;
 
     public InquiryModel(Inquiry inquiry)
     {
+        this.id = new SimpleIntegerProperty();
+        this.inquiryName = new SimpleStringProperty();
+
+        this.id.set(inquiry.getId());
         this.inquiryName.setValue(inquiry.getInquiry());
     }
 
