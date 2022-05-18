f0e0c2511fdd38c2ef9df5629a06c8c9f2acb8bb
Mads Mandahl-Barth <mads-barth@live.dk>
2022-05-18 21:44:46 +0200

    Update CategoryDAO.java

--- src/java/Application/DAL/CategoryDAO.java
+++ src/java/Application/DAL/CategoryDAO.java
@@ -1,24 +1,49 @@
 package Application.DAL;
 
 import Application.BE.Category;
+import Application.BE.Citizen;
+import Application.BE.Group;
+import Application.DAL.DBConnector.DBConnectionPool;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryDAO extends TemplatePatternDAO<Category> {
     @Override
     public Category create(Category input) {
+        System.err.println("illegal");
         return null;
     }
 
     @Override
     public void update(Category input) {
-
+        System.err.println("illegal");
     }
 
     @Override
     public Category read(int id) {
-        return null;
+        String sql = "SELECT * FROM Categories WHERE CatID = ?";
+
+        Connection conn = DBConnectionPool.getInstance().checkOut();
+
+        try {
+            PreparedStatement psas = conn.prepareStatement(sql);
+
+            ResultSet rs = psas.executeQuery();
+            rs.next();
+
+            return new Category(rs.getInt("CatID"), rs.getString("catName"), null);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            DBConnectionPool.getInstance().checkIn(conn);
+        }
     }
 
     @Override
@@ -30,6 +55,12 @@ public class CategoryDAO extends TemplatePatternDAO<Category> {
 
     @Override
     public void delete(int id) {
+        System.err.println("illegal");
+    }
 
+    public static void main(String[] args) {
+        CategoryDAO dao = new CategoryDAO();
+
+        System.out.println(dao.readAll());
     }
 }
